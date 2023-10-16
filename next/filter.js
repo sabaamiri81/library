ce44c9c03304a967e7b078fe4bd64d2cabb5cbee
Saba Amiri <amirisaba043@gmail.com>
2023-10-16 15:23:27 +0330

    add genre filtre

--- next/filter.js
+++ next/filter.js
@@ -1,35 +1,43 @@
 // initial authors
-let uniqueAuthors = [];
-let temp = []
-let count = 0;
+// let uniqueAuthors = [];
+// let temp = []
+// let count = 0;
 
-temp = BOOKS.map(item => {
-    return item.author;
-});
+// temp = BOOKS.map(item => {
+//     return item.author;
+// });
 
-for (let i = 0; i < temp.length; i++) {
+// for (let i = 0; i < temp.length; i++) {
 
-    uniqueAuthors.push(temp[i]);
-    for (let j = i + 1; j < temp.length; j++) {
-        if (temp[i] == temp[j]) {
-            temp.splice(j, 1)
-            j--;
-        }
+//     uniqueAuthors.push(temp[i]);
+//     for (let j = i + 1; j < temp.length; j++) {
+//         if (temp[i] == temp[j]) {
+//             temp.splice(j, 1)
+//             j--;
+//         }
 
-    }
+//     }
 
-}
+// }
 
-temp = uniqueAuthors.map(item => {
-    return `<li>
-          <button class="mainSection__filter--btn">
-                ${item}
-                </button>
-            </li>`
-}).join("");
+// temp = uniqueAuthors.map(item => {
+//     return `<li>
+//           <button class="mainSection__filter--btn" onclick="findAuthor(${item})">
+//                 ${item}
+//                 </button>
+//             </li>`
+// }).join("");
 
 
-filter.innerHTML = temp;
+// filter.innerHTML = temp;
+
+// let filterCards = []
+// function findAuthor(name){
+//      let selectedAuthor= BOOKS.filter(book => book.author == name)
+//     filterCards.push(selectedAuthor)
+// }
+
+// console.log(filterCards);
 
 
 
@@ -56,27 +64,59 @@ filter.innerHTML = temp;
 
 // filter.innerHTML = temp;
 
+function showAllBook(findGenre) {
+    let template =findGenre.map((book) => {
+      const { id, title, author, published_date, language, genre, imgSrc } = book;
+      return `<div class="library__product">
+          <img class="library__product--img" src="./image/${imgSrc}" alt="book picture">
+  <div class="library__product--shadow">
+    <h1 class="library__product--shadow--title">${title}</h1>
+    <h2 class="library__product--shadow--author">  اثر : ${author}</h2>
+    <h3 class="library__product--shadow--date">${published_date}</h3>
+    <h4 class="library__product--shadow--lang">زبان : ${language} </h4>
+    <h4 class="library__product--shadow--genre">ژانر : ${genre}</h4>
+  </div>
+  <button class="library__btn"  onclick="favorite(${id})">افزودن به کتاب های من</button>
+        </div>`
+    })
+    library.innerHTML = template.join("")
+  }
 
 
-// // initial genre
-// temp = BOOKS.map(item => item.genre);
-// let uniqueGenres = [];
 
-// for (let i = 0; i < temp.length; i++) {
-//     uniqueGenres.push(temp[i]);
-//     for (let j = i + 1; j < temp.length; j++) {
-//         if (temp[i] == temp[j]){
-//             temp.splice(j, 1)
-//             j--;
-//         };
-//     }
-// }
-
-// temp = uniqueGenres.map(item => {
-//     return `<li>
-//                 ${item}
-//             </li>`
-// }).join("");
+function findGenre(genre){
+   let selectedGenre = BOOKS.filter(book => book.genre == genre);
+   console.log(selectedGenre);
+   showAllBook(selectedGenre)
+}
 
 
-// filter.innerHTML = temp;
+
+// initial genre
+temp = BOOKS.map(item => item.genre);
+for (let i = 0; i < temp.length; i++) {
+    uniqueGenres.push(temp[i]);
+    for (let j = i + 1; j < temp.length; j++) {
+        if (temp[i] == temp[j]){
+            temp.splice(j, 1)
+            j--;
+        };
+    }
+}
+
+console.log(uniqueGenres);
+
+temp = uniqueGenres.map(item => {
+    return `<li>
+              <button class="mainSection__filter--btn" onclick="findGenre(\`${item}\`)">
+                    ${item}
+                    </button>
+                </li>`
+}).join("");
+
+
+filter.innerHTML = temp;
+
+
+
+
